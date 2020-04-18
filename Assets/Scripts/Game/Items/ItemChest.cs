8608edafa5772f12329d128a4b5a8d792f1f7d8c
Senzouz <renzoravellia@hotmail.com>
2020-04-18 13:28:35 -0500

    Item pick-up

---
+++ Assets/Scripts/Game/Items/ItemChest.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class ItemChest : MonoBehaviour
+{
+    [SerializeField] Item item;
+    [SerializeField] Inventory inventory;
+
+
+    private bool isInRange;
+
+    private void OnValidate()
+    {
+        if(inventory == null)
+            inventory = FindObjectOfType<Inventory>();
+    }
+
+    private void Update()
+    {
+        if (isInRange)
+        {
+                inventory.AddItem(item);
+                Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        CheckCollision(other.gameObject, true);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        CheckCollision(other.gameObject, false);
+    }
+
+    private void CheckCollision(GameObject gameObject, bool state)
+    {
+        if (gameObject.CompareTag("Player"))
+            isInRange = state;
+    }
+}
